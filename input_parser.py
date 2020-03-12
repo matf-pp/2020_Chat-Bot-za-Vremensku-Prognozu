b9822653046bf09bdc4ba095705ba266f7da9ae6
Aleksa Tesic <tesic@getpenta.com>
2020-03-13 00:51:33 +0100

    add first regex based check

--- input_parser.py
+++ input_parser.py
@@ -1,3 +1,5 @@
+import re
+
 from backend import (
     get_by_lat_lon,
     get_by_city_name
@@ -7,15 +9,28 @@ def is_exit_command(user_input):
     return user_input.lower().strip() == 'exit'
 
 def contains_city_name(user_input):
-    return 'contains_city_name'
+    """
+    Receives user_input 
+
+    Returns city_name or None if the city name is not found
+    """
+
+    regex = re.compile(r'.*weather.*in (?P<city_name>[a-z]+).*', re.I | re.S)
+    match = regex.match(user_input)
+    
+    if match:
+        return match.group('city_name'))
+
+    return None
+
 
 def contains_lat_lon(user_input):
     return 'contains_lat_lon'
 
 def determine_response(user_input):
-    params = contains_lat_lon(user_input)
-    if params:
-        return get_by_lat_lon(params)
+    # params = contains_lat_lon(user_input)
+    # if params:
+    #     return get_by_lat_lon(params)
 
     params = contains_city_name(user_input)
     if contains_city_name(user_input):
