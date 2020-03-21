fb16f31c58d3f74b8232ee375f8db932c89a8c83
AizenAngel <boza.mitrovic@gmail.com>
2020-03-21 22:49:08 +0100

    Making backend more readable

--- backend.py
+++ backend.py
@@ -2,23 +2,27 @@ from urllib.parse import urljoin
 import requests
 import os
 from dotenv import load_dotenv
-from json_parser import parse_json
+from json_parser import (CompleteWeatherInfo, parse_json)
 
 load_dotenv()
 
 KEY = os.getenv("KEY")
 URL = os.getenv("URL")
 
-KELVIN_TO_DEGREES_CONST = 273.15
-PRECISION = 2
 
-def kelvins_to_degrees(obj):
-    obj.main.temp       = round(obj.main.temp - KELVIN_TO_DEGREES_CONST, PRECISION)
-    obj.main.feels_like = round(obj.main.feels_like - KELVIN_TO_DEGREES_CONST, PRECISION)
-    obj.main.temp_min   = round(obj.main.temp_min - KELVIN_TO_DEGREES_CONST, PRECISION)
-    obj.main.temp_max   = round(obj.main.temp_max - KELVIN_TO_DEGREES_CONST, PRECISION)
+def scale_temperature(temp):   
+    
+    return round(temp - 273.15, 2)
+
+
+def kelvins_to_degrees(obj:CompleteWeatherInfo):
+    obj.main.temp       = scale_temperature(obj.main.temp)
+    obj.main.feels_like = scale_temperature(obj.main.feels_like)
+    obj.main.temp_min   = scale_temperature(obj.main.temp_min)
+    obj.main.temp_max   = scale_temperature(obj.main.temp_max)
 
     return obj
+
 
 def get_by_city_name(params):
     city_name = params
@@ -26,14 +30,17 @@ def get_by_city_name(params):
     res = requests.get(req)
     weather_obj = parse_json(res.json())
     weather_obj_scaled = kelvins_to_degrees(weather_obj)
+    
     return weather_obj_scaled
    
+
 def get_by_lat_lon(params):
     lat, lon = params
     req = urljoin(URL, f"?lat={lat}&lon={lon}&appid={KEY}")
     res = requests.get(req)
     weather_obj = parse_json(res.json())
     weather_obj_scaled = kelvins_to_degrees(weather_obj)
+    
     return weather_obj_scaled
     
 if __name__ == "__main__":
