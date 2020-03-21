9db0b63eafdbf38cb78814341060c1fd9f3df052
Aleksa Tesic <aleksatesicteske@gmail.com>
2020-03-21 20:31:36 +0100

    Use pack instead of grid

--- gui.py
+++ gui.py
@@ -7,22 +7,24 @@ def init_gui():
     root.title('Weather ChatBot')
 
     frame_root = ttk.Frame(root)
-    frame_root.grid(column = 0, row = 0)
+    frame_root.pack(fill = BOTH, expand = TRUE)
 
+    #? TextBox
     frame_chat_history = ttk.Frame(root, padding = (10, 10, 10, 10))
-    frame_send_message = ttk.Frame(root, padding = (10, 0, 10, 10))
-
-    frame_chat_history.grid(column = 0, row = 0)    
-    frame_send_message.grid(column = 0, row = 1)
+    frame_chat_history.pack(side = TOP, fill = BOTH, expand = TRUE)    
 
     text_chat_history = Text(frame_chat_history)
-    text_chat_history.grid(column = 0, row = 0)
+    text_chat_history.pack(fill = BOTH, expand = TRUE)
+    
+    #? Send Message
+    frame_send_message = ttk.Frame(root, padding = (10, 0, 10, 10))
+    frame_send_message.pack(side = BOTTOM, fill = BOTH, expand = TRUE)
 
     entry_send_message = ttk.Entry(frame_send_message)
-    entry_send_message.grid(column = 0, row = 0)
+    entry_send_message.pack(side = LEFT, fill = BOTH, expand = TRUE)
 
     btn_send_message = ttk.Button(frame_send_message, text = 'Send')
-    btn_send_message.grid(column = 1, row = 0)
+    btn_send_message.pack(side = RIGHT, fill = BOTH, padx = 10)
     
 
     root.mainloop()
