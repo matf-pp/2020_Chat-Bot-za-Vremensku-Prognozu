0dba13687d88cd5046ba985bf72ad1a142768235
Bozidar Mitrovic <33942169+AizenAngel@users.noreply.github.com>
2020-03-21 23:31:49 +0100

    Changing the name of class Main to MainInfo

--- json_parser.py
+++ json_parser.py
@@ -2,16 +2,19 @@ import json
 from pydantic import BaseModel
 from typing import List
 
+
 class Coord(BaseModel):
     lon : float = None
     lat : float = None
 
+        
 class WeatherInfo(BaseModel):
     id          : int = None
     main        : str = None
     description : str = None
     icon        : str = None
 
+        
 class MainInfo(BaseModel):
     temp       : float = None
     feels_like : float = None
@@ -20,10 +23,11 @@ class MainInfo(BaseModel):
     pressure   : float = None
     humidity   : float = None
 
+        
 class Wind(BaseModel):
     speed : float = None
     deg   : float = None
-
+        
 
 class CompleteWeatherInfo(BaseModel):
     coord      : Coord
