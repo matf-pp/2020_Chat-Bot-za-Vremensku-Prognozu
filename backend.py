52c0783280b74416952c2f246dbd17144850ad88
AizenAngel <boza.mitrovic@gmail.com>
2020-03-23 18:36:54 +0100

    Adding units of measure to temperature and wind speed

--- backend.py
+++ backend.py
@@ -15,6 +15,24 @@ def scale_temperature(temp: int) -> int:
     return round(temp - 273.15, 2)
 
 
+def convert_to_string_and_add_units(val: int, unit:str)->str:
+    str_val = str(val) + unit
+
+    return str_val
+
+
+def add_units(obj: CompleteWeatherInfo) -> object:
+    obj.main.temp       = convert_to_string_and_add_units(obj.main.temp, " °C")
+    obj.main.feels_like = convert_to_string_and_add_units(obj.main.feels_like, " °C")
+    obj.main.temp_min   = convert_to_string_and_add_units(obj.main.temp_min,  " °C")
+    obj.main.temp_max   = convert_to_string_and_add_units(obj.main.temp_max, " °C")
+
+    obj.main.humidity   = convert_to_string_and_add_units(obj.main.humidity, " %")
+    obj.wind.speed      = convert_to_string_and_add_units(obj.wind.speed, " m/s")
+
+    return obj
+
+
 def kelvins_to_degrees(obj:CompleteWeatherInfo) -> CompleteWeatherInfo:
     obj.main.temp       = scale_temperature(obj.main.temp)
     obj.main.feels_like = scale_temperature(obj.main.feels_like)
@@ -31,12 +49,18 @@ def make_request(req: str) -> CompleteWeatherInfo:
     return weather_obj
 
 
+def get_readable_weather(obj: CompleteWeatherInfo) -> object:
+    weather_obj_scaled = kelvins_to_degrees(obj)
+
+    return add_units(weather_obj_scaled)
+
+
 def get_by_city_name(params: str) -> CompleteWeatherInfo:
     city_name   = params
     req         = urljoin(URL, f"?q={city_name}&appid={KEY}")    
     weather_obj = make_request(req)
     
-    return kelvins_to_degrees(weather_obj)
+    return get_readable_weather(weather_obj)
    
 
 def get_by_lat_lon(params: tuple) -> CompleteWeatherInfo:
@@ -44,8 +68,7 @@ def get_by_lat_lon(params: tuple) -> CompleteWeatherInfo:
     req         = urljoin(URL, f"?lat={lat}&lon={lon}&appid={KEY}")
     weather_obj = make_request(req)
     
-    return kelvins_to_degrees(weather_obj)
-    
+    return get_readable_weather(weather_obj)
     
 if __name__ == "__main__":
     pass
