06c00d6a84386bf8ceb0c56cc83694bc2817101e
Bozidar Mitrovic <33942169+AizenAngel@users.noreply.github.com>
2020-03-27 13:02:03 +0100

    Adding requests and JSON parsers for multiple city ids and cities in circle area.
    Also, changing names for JSON parsers to make them more understandable

--- json_parser.py
+++ json_parser.py
@@ -1,18 +1,32 @@
 import json
-from models.OneCityWeatherParser import CompleteWeatherInfo
-from models.LatLonCitiesWeatherParser import CompleteWeatherInfo
+from models.OneCityWeatherParser      import CompleteWeatherInfo1  
+from models.LatLonCitiesWeatherParser import CompleteWeatherInfoLL
+from models.CityInCircleWeatherParser import CompleteWeatherInfoR
+from models.ManyCitiesWeatherParser   import CompleteWeatherInfoM
 
-def parse_1_json(JSON):
-    weather_info = CompleteWeatherInfo.parse_obj(JSON)
+
+def parse_1_json(JSON)->CompleteWeatherInfo1:
+    weather_info = CompleteWeatherInfo1.parse_obj(JSON)
+    
     return weather_info
 
-def parse_ll_json(JSON):
-    weather_info = CompleteWeatherInfo.parse_obj(JSON)
+
+def parse_ll_json(JSON)->CompleteWeatherInfoLL:
+    weather_info = CompleteWeatherInfoLL.parse_obj(JSON)
+    
     return weather_info
 
 
-def parse_r_json(JSON):
-    pass
+def parse_r_json(JSON)->CompleteWeatherInfoR:
+    weather_info = CompleteWeatherInfoR.parse_obj(JSON)
+    
+    return weather_info
+
+
+def parse_m_json(JSON)->CompleteWeatherInfoM:
+    weather_info = CompleteWeatherInfoM.parse_obj(JSON)
+
+    return weather_info
 
 
 if __name__ == "__main__":
