d1c310cb7d65035746b7d9c7091fb350cef72ea7
Bozidar Mitrovic <33942169+AizenAngel@users.noreply.github.com>
2020-03-27 13:06:09 +0100

    Adding requests and JSON parsers for multiple city ids and cities in circle area.
    Also, changing names for JSON parsers to make them more understandable

---
+++ models/OneCityWeatherParser.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Coord(BaseModel):
+    lon : float = None
+    lat : float = None
+
+        
+class WeatherInfo(BaseModel):
+    id          : int = None
+    main        : str = None
+    description : str = None
+    icon        : str = None
+
+        
+class MainInfo(BaseModel):
+    temp       : float = None
+    feels_like : float = None
+    temp_min   : float = None
+    temp_max   : float = None
+    pressure   : float = None
+    humidity   : float = None
+
+        
+class Wind(BaseModel):
+    speed : float = None
+    deg   : float = None
+        
+
+class CompleteWeatherInfo1(BaseModel):
+    coord      : Coord
+    weather    : List[WeatherInfo]
+    main       : MainInfo
+    wind       : Wind
